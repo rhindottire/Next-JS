f4a1c3604eed8798083bdf951a9c782295e61a31
Achmad Ridho Fa'iz <achmadaliridho46@gmail.com>
2026-02-03 10:26:13 +0700

    groundwork for product page, API route, and firebase integration.

--- pages-router/src/pages/product/index.tsx
+++ pages-router/src/pages/product/index.tsx
@@ -1,18 +1,39 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
+type productType = {
+  id: number;
+  name: string;
+  price: number;
+  size: string;
+}
+
 const ProductPage = () => {
-  const [ isLogin, setLogin ] = useState(false);
+  const [ isLogin ] = useState(true);
   const { push } = useRouter();
+  const [products, setProducts] = useState([]);
+
   useEffect(() => {
     if(!isLogin) {
       push("/auth/login");
     }
   }, []);
 
+  useEffect(() => {
+    fetch("/api/product")
+      .then((res) => res.json())
+      .then((response) => {
+        setProducts(response.data)
+        console.log(response)
+      });
+  }, []);
+
   return (
     <div>
       <h1>Product Page</h1>
+      {products.map((product: productType) => (
+        <div key={product.id}>{product.name}</div>
+      ))}
     </div>
   );
 };
